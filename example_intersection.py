95dd581444c447e42521057509d2894403719d8f
Marc Schlichting <schlichting.marc@gmail.com>
2022-11-15 16:34:56 -0800

    intersection scenario

--- example_intersection.py
+++ example_intersection.py
@@ -183,17 +183,19 @@ class OthogonalIntersectionScenario(object):
         failure_configs = []
         non_failure_configs = []
 
-        with WorkerPool(n_jobs=num_processes) as pool:
 
-            for result in pool.imap(self.inner_loop_one_step, num_trials * [(simulation_configuration,)],progress_bar=True):
-            # result = self.inner_loop_one_step()
 
-                if result[0] is not None:
-                    failures.append(result[0])
-                if result[1] is not None:
-                    failure_configs.append(result[1])
-                if result[2] is not None:
-                    non_failure_configs.append(result[2])
+        # with WorkerPool(n_jobs=num_processes) as pool:
+
+        #     for result in pool.imap(self.inner_loop_one_step, num_trials * [(simulation_configuration,)],progress_bar=True):
+        #     # result = self.inner_loop_one_step()
+
+        #         if result[0] is not None:
+        #             failures.append(result[0])
+        #         if result[1] is not None:
+        #             failure_configs.append(result[1])
+        #         if result[2] is not None:
+        #             non_failure_configs.append(result[2])
         
 
         for i in range(num_trials):
