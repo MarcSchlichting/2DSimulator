89833a152d82b8df362a2d12ee8245f670e14bf2
Marc Schlichting <mschlichting@DNa82844d.SUNet>
2022-11-21 23:40:57 -0800

    200 without sem

--- meta_training.py
+++ meta_training.py
@@ -103,8 +103,8 @@ def objective(cf_simulation_config):
     # if compute_cost>compute_budget:
     #     mse_mean += 10000
     
-    # return {"mse":(mse_mean.item(),mse_sem.item()),"compute_cost":(compute_cost,0.0)}   #sem version
-    return {"mse":(mse_mean.item(),0.0),"compute_cost":(compute_cost,0.0)}   #non-sem version
+    return {"mse":(mse_mean.item(),mse_sem.item()),"compute_cost":(compute_cost,0.0)}   #sem version
+    # return {"mse":(mse_mean.item(),0.0),"compute_cost":(compute_cost,0.0)}   #non-sem version
 
 
 if __name__=="__main__":
@@ -145,11 +145,16 @@ if __name__=="__main__":
         # Local evaluation here can be replaced with deployment to external system.
         raw_data = objective(parameters)
         ax_client.complete_trial(trial_index=trial_index, raw_data=raw_data)
-        ax_client.generation_strategy.trials_as_df.to_csv("meta_training_200_200.csv")
-        best_parameters, values = ax_client.get_best_parameters()
-        best_parameter_list.append(best_parameters)
-        pd.DataFrame.from_records(best_parameter_list).to_csv("meta_training_200_200_best_parameters.csv")
-        print("Best Parameters: ",best_parameters)
+        ax_client.generation_strategy.trials_as_df.to_csv("meta_training_200_200_sem.csv")
+        try:
+            best_parameters, values = ax_client.get_best_parameters()
+            best_parameter_list.append(best_parameters)
+            pd.DataFrame.from_records(best_parameter_list).to_csv("meta_training_200_200_sem_best_parameters.csv")
+            print("Best Parameters: ",best_parameters)
+        except:
+            best_parameter_list.append({"dt": None, "integration_method": None, "sensor_std": None})
+            pd.DataFrame.from_records(best_parameter_list).to_csv("meta_training_200_200_sem_best_parameters.csv")
+            print("no best parameter")
     
     best_parameters, values = ax_client.get_best_parameters()
 
