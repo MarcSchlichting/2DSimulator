220380afe7d6921af0a5c868d6afd55fe1b29bb6
Marc Schlichting <mschlichting@DNa82844d.SUNet>
2022-11-21 20:24:16 -0800

    updated meta_training

--- meta_training.py
+++ meta_training.py
@@ -145,10 +145,10 @@ if __name__=="__main__":
         # Local evaluation here can be replaced with deployment to external system.
         raw_data = objective(parameters)
         ax_client.complete_trial(trial_index=trial_index, raw_data=raw_data)
-        ax_client.generation_strategy.trials_as_df.to_csv("meta_training_200.csv")
+        ax_client.generation_strategy.trials_as_df.to_csv("meta_training_200_sem.csv")
         best_parameters, values = ax_client.get_best_parameters()
         best_parameter_list.append(best_parameters)
-        pd.DataFrame.from_records(best_parameter_list).to_csv("meta_training_200_best_parameters.csv")
+        pd.DataFrame.from_records(best_parameter_list).to_csv("meta_training_200_sem_best_parameters.csv")
         print("Best Parameters: ",best_parameters)
     
     best_parameters, values = ax_client.get_best_parameters()
